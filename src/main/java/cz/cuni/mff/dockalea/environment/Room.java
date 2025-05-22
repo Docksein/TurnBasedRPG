c69d558f2357dfff94b2f523f5139e6295082f6c
Adam <adam.dockalek@gmail.com>
2025-05-22 12:09:30 +0200

    Expanded Room class for enemy combat.

--- src/main/java/cz/cuni/mff/dockalea/environment/Room.java
+++ src/main/java/cz/cuni/mff/dockalea/environment/Room.java
@@ -1,5 +1,7 @@
 package cz.cuni.mff.dockalea.environment;
 
+import cz.cuni.mff.dockalea.entities.Enemy;
+
 public class Room {
     public final String Name;
     public final String Description;
@@ -7,6 +9,9 @@ public class Room {
     public final String South;
     public final String East;
     public final String West;
+    private boolean visited;
+    private boolean explored;
+    private Enemy currentEnemy;
 
     public Room(String name, String desc, String north, String south, String east, String west) {
         Name = name;
@@ -15,11 +20,25 @@ public class Room {
         South = south;
         East = east;
         West = west;
+        this.visited = false;
+        this.explored = false;
+        this.currentEnemy = null;
+    }
+
+    public boolean isVisited() { return visited; }
+    public boolean isExplored() { return explored; }
+    public Enemy getCurrentEnemy() { return currentEnemy; }
+
+    public void setVisited(boolean visited) { this.visited = visited; }
+    public void setExplored(boolean explored) { this.explored = explored; }
+    public void setCurrentEnemy(Enemy enemy) { this.currentEnemy = enemy; }
+
+    public boolean isBossRoom() {
+        return Name.toLowerCase().contains("boss");
     }
 
     @Override
     public String toString() {
         return Name + ", N: " + North + ", S: " + South + ", E:" + East + ", W: " + West;
     }
-
 }
